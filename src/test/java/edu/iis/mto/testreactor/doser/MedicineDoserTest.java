8511d336b946098d256799fe5044bfee7b4069a3
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-05-13 16:55:17 +0200

    added missing tests

--- src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -2,12 +2,18 @@ package edu.iis.mto.testreactor.doser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 import edu.iis.mto.testreactor.doser.infuser.Infuser;
+import edu.iis.mto.testreactor.doser.infuser.InfuserException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.concurrent.TimeUnit;
@@ -66,6 +72,47 @@ class MedicineDoserTest {
 		assertThrows(UnavailableMedicineException.class, () -> medicineDoserToTest.dose(incorrectReceipeUnavailableMedicine));
 	}
 
+	@Test
+	void someErrorOccuredExpectingStatusError() {
+		//ten test próbuje przetestowac linijki 38-40 nie mam innego pomyslu jak wyrzucic tam
+		//exception
+		doThrow(NullPointerException.class).when(dosageLogMock).logStart();
+		medicineDoserToTest.add(defaultMedicinePackage);
+		DosingResult dosingResult = medicineDoserToTest.dose(defaultReceipe);
+		assertEquals(DosingResult.ERROR, dosingResult);
+	}
 
+	@Test
+	void infuserExceptionExpectingLogDiffuserErrorToBeCalled() throws InfuserException {
+		doThrow(InfuserException.class).when(infuserMock).dispense(any(), any());
+		InOrder order = Mockito.inOrder(dosageLogMock);
+		medicineDoserToTest.add(defaultMedicinePackage);
+		medicineDoserToTest.dose(defaultReceipe);
+		order.verify(dosageLogMock).logStart();
+		order.verify(dosageLogMock).logStartDose(any(),any());
+		order.verify(dosageLogMock).logDifuserError(any(),any());
+	}
 
+	@Test
+	void clockShouldBeCalledThreeTimes() {
+		medicineDoserToTest.add(defaultMedicinePackage);
+		medicineDoserToTest.dose(defaultReceipe);
+		verify(clockMock,times(defaultReceipe.getNumber())).wait((defaultReceipe.getDose().getPeriod()));
+	}
+
+	@Test
+	void orderOfCallsInSuccessfulRun() throws InfuserException {
+		InOrder order = Mockito.inOrder(dosageLogMock,infuserMock,clockMock);
+		medicineDoserToTest.add(defaultMedicinePackage);
+		medicineDoserToTest.dose(defaultReceipe);
+		order.verify(dosageLogMock).logStart();
+		for (int i = 0; i < defaultReceipe.getNumber(); i++) {
+			order.verify(dosageLogMock).logStartDose(any(), any());
+			order.verify(infuserMock).dispense(any(), any());
+			order.verify(dosageLogMock).logEndDose(any(), any());
+			order.verify(clockMock).wait(any());
+		}
+		order.verify(dosageLogMock).logEnd();
+
+	}
 }
