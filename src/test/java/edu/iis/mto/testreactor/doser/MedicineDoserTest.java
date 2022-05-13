1684fe67773af44cbf34f3633873dc71d7a8bb1f
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-05-13 02:51:16 +0200

    setting up and running first test

--- src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -2,15 +2,47 @@ package edu.iis.mto.testreactor.doser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import edu.iis.mto.testreactor.doser.infuser.Infuser;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.TimeUnit;
 
 @ExtendWith(MockitoExtension.class)
 class MedicineDoserTest {
+	@Mock
+	private Infuser infuserMock;
+	@Mock
+	private DosageLog dosageLogMock;
+	@Mock
+	private Clock clockMock;
+	private MedicineDoser medicineDoserToTest;
+	private Capacity defaultCapacity = Capacity.of(100, CapacityUnit.MILILITER);
+	private Capacity tenthOfDefaultCapacity = Capacity.of(10, CapacityUnit.MILILITER);
+	private Dose defaultDose = Dose.of(tenthOfDefaultCapacity, Period.of(1, TimeUnit.HOURS));
+	private Medicine defaultMedicine = Medicine.of("default medicine");
+	private MedicinePackage defaultMedicinePackage = MedicinePackage.of(defaultMedicine, defaultCapacity);
+	private Receipe defaultReceipe = Receipe.of(defaultMedicine, defaultDose, 3);
 
-    @Test
-    void itCompiles() {
-        assertEquals(2, 1 + 1);
-    }
+	@BeforeEach
+	void setUp() {
+		medicineDoserToTest = new MedicineDoser(infuserMock, dosageLogMock, clockMock);
+	}
+
+	@Test
+	void itCompiles() {
+		assertEquals(2, 1 + 1);
+	}
+
+	@Test
+	void successfulRunWithNoErrors() {
+		medicineDoserToTest.add(defaultMedicinePackage);
+		medicineDoserToTest.dose(defaultReceipe);
+		assertEquals(2, 1 + 1);
+	}
+
+
 }
