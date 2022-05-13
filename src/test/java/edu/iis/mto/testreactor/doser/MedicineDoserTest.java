f6f8ee3201d6e5ce3ee1f5d10ccaf02259b6c70b
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-05-13 15:57:49 +0200

    fixed succesfulrun test, added test for incorrect receipe

--- src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.testreactor.doser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import edu.iis.mto.testreactor.doser.infuser.Infuser;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,9 +41,31 @@ class MedicineDoserTest {
 	@Test
 	void successfulRunWithNoErrors() {
 		medicineDoserToTest.add(defaultMedicinePackage);
-		medicineDoserToTest.dose(defaultReceipe);
-		assertEquals(2, 1 + 1);
+		DosingResult dosingResult = medicineDoserToTest.dose(defaultReceipe);
+		assertEquals(DosingResult.SUCCESS, dosingResult);
 	}
+
+	@Test
+	void incorrectRecipeWrongNumberExpectingIllegalArgumentException() {
+		medicineDoserToTest.add(defaultMedicinePackage);
+		Receipe incorrectReceipeWrongNumber = Receipe.of(defaultMedicine, defaultDose, 0);
+		assertThrows(IllegalArgumentException.class, () -> medicineDoserToTest.dose(incorrectReceipeWrongNumber));
+	}
+
+	@Test
+	void incorrectRecipeWrongDoseExpectingInsufficient() {
+		medicineDoserToTest.add(defaultMedicinePackage);
+		Receipe incorrectReceipeNotEnoughMedicine = Receipe.of(defaultMedicine, defaultDose, 11);
+		assertThrows(InsufficientMedicineException.class, () -> medicineDoserToTest.dose(incorrectReceipeNotEnoughMedicine));
+	}
+
+	@Test
+	void incorrectRecipeWrongMedicineExpectingUnavailableMedicineException() {
+		medicineDoserToTest.add(defaultMedicinePackage);
+		Receipe incorrectReceipeUnavailableMedicine = Receipe.of(Medicine.of("UNAVAILABLE_MEDICINE"), defaultDose, 1);
+		assertThrows(UnavailableMedicineException.class, () -> medicineDoserToTest.dose(incorrectReceipeUnavailableMedicine));
+	}
+
 
 
 }
